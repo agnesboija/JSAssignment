ce145953186a75e754367c3a561ebb4e47a23070
agnes <agnesannaboija@gmail.com>
2025-10-01 10:28:28 +0200

    Update js.app
    lagt till lite kommentarer

--- js/app.js
+++ js/app.js
@@ -5,9 +5,22 @@ console.log(
 
 console.log("Hej agnes, hur är läget?");
 console.log("This is line 1.");
+//1/10-25
 console.log('this is line 2.');
+// agnesannaboija@gmail.com
 console.log('This is line 3.'); console.log('this is line 4.');
+/*
+för fler rader av kommentarer
+använd stjärna och dash
+ */
 console.log('this is line 5.');;
+// agnes boija
 
 console.log('this is line 6.');
+//Detta ska ej printas
+/*
+
+console.log('hejhej')
+
+ */
 
