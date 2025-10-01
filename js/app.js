b66189badb17a500f9effe23a9d50869a06df896
agnes <agnesannaboija@gmail.com>
2025-10-01 10:16:31 +0200

    ThisIsLine
    test

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ console.log(
 );
 
 console.log("Hej agnes, hur är läget?");
+console.log("This is line 1.");
+console.log('this is line 2.');
+console.log('This is line 3.'); console.log('this is line 4.');
+console.log('this is line 5.');;
 
-
+console.log('this is line 6.');
 
